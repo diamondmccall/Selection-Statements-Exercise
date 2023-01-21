04712f3c6d0ca4f1949f56a56858a5377fdbae93
Diamond Mccall <singlikediamond@gmail.com>
2023-01-20 23:49:37 -0600

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("Welcome to the Guessing Game!");
+            Console.WriteLine("Try to guess my favorite number!");
+
+
+
+            var r = new Random();
+
+            var favNumber = r.Next(1, 1000);
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput > favNumber)
+
+            {
+                Console.WriteLine("Too high! Not even close!");
+            }
+
+            else if (userInput < favNumber)
+
+
+            {
+                Console.WriteLine("Nevermind! Now you're a little too low!");
+            }
+
+            else if (userInput == favNumber)
+
+
+            {
+                Console.WriteLine("YESSS! GREAT GUESS!");
+            }
+
+
+          
+            
         }
     }
 }
